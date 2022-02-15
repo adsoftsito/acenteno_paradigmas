abc3f4bc84dea5d38b7068ba6b5bd7da35a5ebd1
adsoftsito <adsoft@live.com.mx>
2022-02-15 12:10:33 -0600

    add matriz_cuadrada.m

---
+++ matlab/matriz_cuadrada.m
@@ -0,0 +1,20 @@
+clear, clc
+
+% matriz cuadrada de orden 3 x 3
+A = [1 2 3;
+     4 5 6;
+     6 7 8];
+     
+ renglones = 3;
+ columnas = 3;
+ 
+ % impresion de matriz usando 2 ciclos anidados
+ 
+fprintf('Impresion de matriz de orden 3x3 \n');
+
+for ren=1:renglones;
+    for col=1:columnas;
+        fprintf('%d ', A(ren, col))
+    end
+    fprintf('\n');
+end
